821af4f5b14ae35e569ac4cb8d7dc40cf7a80d0a
Jonas Van Haeken <jonas.van.haeken@outlook.be>
2022-05-12 22:12:42 +0200

    Fix email

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,12 @@
 <?php
-  $to = 'jonas.van.haeken@outlook.Be';
+  $to = 'jonas.van.haeken@outlook.be';
   $subject = $_POST['subject'];
   $message = $_POST['message'];
   $from_email = $_POST['email'];
   $from_name = $_POST['name'];
 
   $header = "From:$from_email\r\n";
+  $header .= "CC:$from_email\r\n";
   $header .= "MIME-Version: 1.0\r\n";
   $header .= "Content-type: text/html\r\n";
 
